13755f197b8455adc9b531231e7f15ae9f08e9e1
mjdeuzeman <50409412+mjdeuzeman@users.noreply.github.com>
2020-06-22 14:35:07 +0200

    Add files via upload

--- Dht/DHT11multi.ino
+++ Dht/DHT11multi.ino
@@ -20,52 +20,42 @@
 
 #include "DHT.h"
 
-#define DHTPIN 0     // Digital pin connected to the DHT sensor
-#define DHTPIN2 5     // Digital pin connected to the DHT sensor
+#define DHTPIN1 2     // Digital pin connected to the DHT sensor
+#define DHTPIN2 7     // Digital pin connected to the DHT sensor
 #define DHTPIN3 4     // Digital pin connected to the DHT sensor
 
 #define DHTTYPE DHT11   // DHT 11
 
-DHT dht(DHTPIN, DHTTYPE); // multiple DHT-sensors possible
-DHT dht2(DHTPIN2, DHTTYPE); // multiple DHT-sensors possible
-DHT dht3(DHTPIN3, DHTTYPE); // multiple DHT-sensors possible
+DHT dht[] = {
+  {DHTPIN1, DHTTYPE},
+  {DHTPIN2, DHTTYPE},
+  {DHTPIN3, DHTTYPE},
+};
+
+float h[3];
+float t[3];
 
 void setup() {
   Serial.begin(9600);
-  dht.begin();
+  for (auto& sensor : dht) {
+    sensor.begin();
+  }
 }
 
 void loop() {
   // Wait at least 2 seconds for DHT11
   delay(2000);
 
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default, insert 'true' between brackets for Fahrenheit)
-  float t = dht.readTemperature();
-
-  float h2 = dht2.readHumidity();
-  // Read temperature as Celsius (the default, insert 'true' between brackets for Fahrenheit)
-  float t2 = dht2.readTemperature();
-
-  float h3 = dht3.readHumidity();
-  // Read temperature as Celsius (the default, insert 'true' between brackets for Fahrenheit)
-  float t3 = dht3.readTemperature();
-
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
-    return;
+  for (int i = 0; i < 3; i++) {
+    t[i] = dht[i].readTemperature();
+    h[i] = dht[i].readHumidity();
   }
 
-  Serial.print(h);
+  for (int i = 0; i < 3; i++) {
+  Serial.print(h[i]);
   Serial.print(F("\t"));
-  Serial.print(t);
+  Serial.print(t[i]);
   Serial.print(F("\t"));
-  Serial.print(h2);
-  Serial.print(F("\t"));
-  Serial.print(t2);  
-  Serial.print(F("\t"));
-  Serial.print(h3);
-  Serial.print(F("\t"));
-  Serial.println(t3);
+  }
+  Serial.print("\n");
 }
