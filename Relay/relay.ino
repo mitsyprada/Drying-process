ac445369738972a49e1e3bf0ef20d635a90e5ae3
Natanael M Gomes <50499628+natanaelmgomes@users.noreply.github.com>
2020-06-30 14:34:21 +0200

    adapted code

--- Relay/relay.ino
+++ Relay/relay.ino
@@ -1,36 +1,37 @@
-int ThermistorPin = 0;
-int Vo;
-float R1 = 100000;
-float logR2, R2, T, Tc, Tf;
-float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
-int relay = 2;
+int tempheatbed_pin = 6;
+int Vo_bed;
+float logR2_bed, T_bed, Tc_bed;
+int relay_pin = 7;
 volatile byte relayState = LOW;
-int state = LOW;   
+
+float R1 = 100000;
+float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
 void setup() {
-  pinMode(relay, OUTPUT);
-  digitalWrite(relay, HIGH);
+  pinMode(relay_pin, OUTPUT);
+  digitalWrite(relay_pin, LOW);
   Serial.begin(9600);
 }
 
 void loop() {
-  Vo = analogRead(ThermistorPin);
-  logR2 = log(R1 * ((1024.0 / (float)Vo) - 1.0));
-  T = 1.0 / (c1 + (c2*logR2) + (c3*logR2*logR2*logR2));
-  Tc = T - 273.15;
+  Vo_bed = analogRead(tempheatbed_pin);
+  logR2_bed = log(R1 * ((1024.0 / (float)Vo_bed) - 1.0));
+  T_bed = 1.0 / (c1 + (c2*logR2_bed) + (c3*logR2_bed*logR2_bed*logR2_bed));
+  Tc_bed = T_bed - 273.15;
   Serial.print("Temperature: "); 
-  Serial.print(Tc);
+  Serial.print(Tc_bed);
   Serial.println(" C");   
   delay(500);
 
-  if (Tc <= 80) {          // check if the sensor is HIGH   
+  // Code to control the temperature in the range
+  if (Tc_bed <= 80) {          // check if the sensor is HIGH   
     if(relayState == LOW){
-      digitalWrite(relay, HIGH);
+      digitalWrite(relay_pin, HIGH);
     }
     relayState = HIGH;  
     Serial.println("ON");
   } 
-  if (Tc >= 90) {  
-    digitalWrite(relay, LOW);
+  if (Tc_bed >= 90) {  
+    digitalWrite(relay_pin, LOW);
     relayState = LOW;
     Serial.println("OFF");
     }
